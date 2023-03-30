d5ddf9feb2bf2b8fd8ae49490bc787e1e395f587
Mateusz Wilk <matewilk@gmail.com>
2023-03-30 22:10:34 +0100

    (feat): handle openai and endpoint errors

--- packages/api/src/router/openai.test.ts
+++ packages/api/src/router/openai.test.ts
@@ -7,30 +7,75 @@ import type { SignedInAuthObject } from "@clerk/nextjs/api";
 import type { OpenAIApi } from "openai";
 
 describe("openaiRouter", () => {
-  it("chatCompletion", async () => {
+  describe("chatCompletion", () => {
+    let chatCompletionMock: jest.Mock;
     const auth = {
       userId: "123",
     } as SignedInAuthObject;
+    let openai: OpenAIApi;
 
-    const chatCompletionMock = jest.fn().mockResolvedValue({
-      data: {
-        choices: [{ message: { role: "assistant", content: "test" } }],
-      },
+    beforeEach(() => {
+      chatCompletionMock = jest.fn().mockResolvedValue({
+        data: {
+          choices: [{ message: { role: "assistant", content: "test" } }],
+        },
+      });
+      openai = {
+        createChatCompletion: chatCompletionMock,
+      } as unknown as OpenAIApi;
     });
-    const openai = {
-      createChatCompletion: chatCompletionMock,
-    } as unknown as OpenAIApi;
 
-    const ctx = await createContextInner({ auth, prisma, openai });
-    const caller = appRouter.createCaller(ctx);
+    it("should return data", async () => {
+      const ctx = await createContextInner({ auth, prisma, openai });
+      const caller = appRouter.createCaller(ctx);
 
-    type Input = inferProcedureInput<typeof openaiRouter.chatCompletion>;
-    const input: Input = "Hello";
+      type Input = inferProcedureInput<typeof openaiRouter.chatCompletion>;
+      const input: Input = "Hello";
 
-    const result = await caller.openai.chatCompletion(input);
+      const result = await caller.openai.chatCompletion(input);
 
-    expect(result).toMatchObject({
-      message: { role: "assistant", content: "test" },
+      expect(result).toMatchObject({
+        message: { role: "assistant", content: "test" },
+      });
+    });
+
+    it("should return generic error when something goes wrong", async () => {
+      chatCompletionMock.mockRejectedValue(new Error("test"));
+
+      const ctx = await createContextInner({ auth, prisma, openai });
+      const caller = appRouter.createCaller(ctx);
+
+      type Input = inferProcedureInput<typeof openaiRouter.chatCompletion>;
+      const input: Input = "Hello";
+
+      const result = await caller.openai.chatCompletion(input);
+
+      expect(result).toMatchObject({
+        message: "An error occurred while processing the request: test",
+      });
+    });
+
+    it("should return API error when something goes wrong", async () => {
+      chatCompletionMock.mockRejectedValue({
+        status: 400,
+        code: "test code",
+        message: "test message",
+      });
+
+      const ctx = await createContextInner({ auth, prisma, openai });
+      const caller = appRouter.createCaller(ctx);
+
+      type Input = inferProcedureInput<typeof openaiRouter.chatCompletion>;
+      const input: Input = "Hello";
+
+      const result = await caller.openai.chatCompletion(input);
+
+      expect(result).toMatchObject({
+        status: 400,
+        code: "test code",
+        message:
+          "An error occurred while processing the OpenAI API request: test message",
+      });
     });
   });
 });
