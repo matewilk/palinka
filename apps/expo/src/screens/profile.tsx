bf9eb06384211083944c861c2e0ec92804a1015d
Mat Wilk <matewilk@gmail.com>
2023-04-25 19:18:05 +0100

    (feat): animate mobile main workflow screens (#9)

--- apps/expo/src/screens/profile.tsx
+++ apps/expo/src/screens/profile.tsx
@@ -6,6 +6,8 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
+import Animated, { FadeInUp, FadeInDown } from "react-native-reanimated";
+
 import { tokens, translate } from "../i18n";
 
 export const ProfileScreen = () => {
@@ -13,51 +15,72 @@ export const ProfileScreen = () => {
     <SafeAreaView>
       <View className="h-full w-full p-4">
         {/* Container for the avatar icon, text, and button */}
-        <View className="flex-1 items-center justify-start pt-24">
-          <View className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-300">
+        <Animated.View
+          entering={FadeInUp.duration(500).springify()}
+          className="flex-1 items-center justify-start pt-24"
+        >
+          <Animated.View
+            entering={FadeInUp.delay(100).duration(500).springify()}
+            className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-300"
+          >
             <Text className="text-4xl font-bold text-gray-500">P</Text>
-          </View>
+          </Animated.View>
 
-          <View className="w-full flex-1 items-center justify-center space-y-4 pt-4">
-            <View className="w-full items-center space-y-1">
+          <Animated.View
+            entering={FadeInDown.delay(200).duration(500).springify()}
+            className="w-full flex-1 items-center justify-center space-y-4 pt-4"
+          >
+            <Animated.View
+              entering={FadeInDown.delay(300).duration(500).springify()}
+              className="w-full items-center space-y-1"
+            >
               <Text className="text-center">
                 {translate(tokens.screens.profile.firstName)}
               </Text>
               <TextInput
-                className="w-full rounded border border-gray-300 py-2 px-3"
+                className="w-full rounded bg-gray-200 py-2 px-3"
                 placeholder="First Name"
               />
-            </View>
+            </Animated.View>
 
-            <View className="w-full items-center space-y-1">
+            <Animated.View
+              entering={FadeInDown.delay(400).duration(500).springify()}
+              className="w-full items-center space-y-1"
+            >
               <Text className="text-center">
                 {translate(tokens.screens.profile.lastName)}
               </Text>
               <TextInput
-                className="w-full rounded border border-gray-300 py-2 px-3"
+                className="w-full rounded bg-gray-200 py-2 px-3"
                 placeholder="Last Name"
               />
-            </View>
+            </Animated.View>
 
-            <View className="w-full items-center space-y-1">
+            <Animated.View
+              entering={FadeInDown.delay(500).duration(500).springify()}
+              className="w-full items-center space-y-1"
+            >
               <Text className="text-center">
                 {translate(tokens.screens.profile.email)}
               </Text>
               <TextInput
-                className="w-full rounded border border-gray-300 py-2 px-3"
+                className="w-full rounded bg-gray-200 py-2 px-3"
                 placeholder="Email Address"
               />
-            </View>
-          </View>
+            </Animated.View>
+          </Animated.View>
 
-          <View className="w-full flex-1 items-center justify-end pb-4">
+          <Animated.View
+            entering={FadeInUp.delay(600).duration(500).springify()}
+            className="w-full flex-1 items-center justify-end pb-4"
+          >
             <TouchableOpacity className="w-full items-center rounded bg-red-400 py-3">
               <Text className="text-center font-bold text-white">
                 {translate(tokens.screens.profile.deleteAccountBtn)}
               </Text>
             </TouchableOpacity>
-          </View>
-        </View>
+          </Animated.View>
+        </Animated.View>
       </View>
     </SafeAreaView>
   );
