a8458add2d244e4b6ddefe0524e1ec028215ad6f
Mat Wilk <matewilk@gmail.com>
2023-05-15 21:28:28 +0100

    (feat): framer motion landing page animations (#13)

--- apps/nextjs/src/components/Features.tsx
+++ apps/nextjs/src/components/Features.tsx
@@ -1,10 +1,12 @@
-const Feature = ({
-  title,
-  description,
-}: {
+import { useRef } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+type Feature = {
   title: string;
   description: string;
-}) => {
+};
+
+const Feature = ({ title, description }: Feature) => {
   return (
     <div className="flex w-full items-center">
       <div className="h-16 w-16 flex-shrink-0 rounded-full bg-gray-300" />
@@ -16,37 +18,52 @@ const Feature = ({
   );
 };
 
-const Features = () => {
+const Features = ({
+  features,
+  title,
+  reverse = false,
+}: {
+  features: Feature[];
+  title: string;
+  reverse?: boolean;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+  const y = useTransform(scrollYProgress, [0.7, 1], ["0%", "100%"]);
+
   return (
     <section className="flex w-full flex-col items-center justify-center py-32 px-4">
-      <h2 className="text-4xl font-bold tracking-tight md:text-5xl">
-        Smart Goodies
-      </h2>
+      <h2 className="text-4xl font-bold tracking-tight md:text-5xl">{title}</h2>
       <div className="flex w-full flex-col items-center justify-center md:flex-row">
-        <div className="flex w-full flex-col items-center justify-center p-10 md:w-1/2">
-          <img
-            src="https://via.placeholder.com/350x550"
-            alt="Placeholder"
-            className="h-[500px] w-72 rounded-3xl"
-          />
+        <div
+          className={`flex w-full flex-col items-center justify-center p-10 md:w-1/2 ${
+            reverse ? "md:order-last" : ""
+          }`}
+        >
+          <div
+            ref={ref}
+            className="flex h-96 w-52 items-center justify-center overflow-hidden rounded-3xl bg-gray-300"
+          >
+            <motion.div
+              style={{ y }}
+              className="flex h-[350px] w-48 items-center justify-center rounded-2xl bg-gray-500"
+            ></motion.div>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-8 p-10 md:w-1/2 md:justify-start md:p-0 md:py-10 md:pr-10">
-          <Feature
-            title="Streamlined Parent-Teacher Communication"
-            description="Effortless and Personalized Messaging for Busy Educators"
-          />
-          <Feature
-            title="Automated Text Correction"
-            description="Error-free Writing and Feedback with AI-driven Editing"
-          />
-          <Feature
-            title="Innovative Lesson Plan Generation"
-            description="Engaging, Tailored Curriculum Design Made Easy for Educators"
-          />
-          <Feature
-            title="Classroom Activity Suggestions"
-            description="Discover Creative, Age-Appropriate Learning Experiences for Your  Students"
-          />
+        <div
+          className={`flex flex-col items-center justify-center space-y-8 p-10 md:w-1/2 md:justify-start md:p-0 md:py-10 md:pr-10 ${
+            reverse ? "md:pr-0 md:pl-24" : "md:pr-10"
+          }`}
+        >
+          {features.map((feature, index) => (
+            <Feature
+              key={index}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
         </div>
       </div>
     </section>
--- apps/nextjs/src/components/Join.tsx
+++ apps/nextjs/src/components/Join.tsx
@@ -1,27 +1,27 @@
+const joinTitle =
+  "Join the waiting list now & be first to access our app's treasures. Sign up today!";
+const inputPlaceholder = "Enter your email";
+const buttonLabel = "Join the waiting list";
+
 const Join = () => {
   return (
     <section className="flex w-full items-center justify-center py-20 px-4">
       <div className="flex w-full flex-col items-center justify-center rounded-xl bg-gray-100 md:flex-row">
         <div className="flex w-full flex-col items-center justify-center pt-10 md:w-1/3 md:p-10">
-          <img
-            src="https://via.placeholder.com/200x200"
-            alt="Placeholder"
-            className="h-48 w-48 rounded-xl"
-          />
+          <div className="h-48 w-48 rounded-xl bg-gray-300" />
         </div>
         <div className="flex flex-col items-start justify-center gap-3 p-10 md:w-2/3 md:pr-10">
           <div className="text-3xl font-bold tracking-tight text-gray-700 md:text-3xl">
-            Join the waiting list now & be first to access our app&apos;s
-            treasures. Sign up today!
+            {joinTitle}
           </div>
           <div className="flex flex-row">
             <input
               type="text"
-              placeholder="Enter your email"
+              placeholder={inputPlaceholder}
               className="focus:shadow-outline mt-2 h-10 w-full rounded-lg rounded-r-none border px-3 text-sm text-gray-700 placeholder-gray-600"
             />
             <button className="mt-2 w-full rounded-lg rounded-l-none bg-black py-2 px-4 font-bold text-white hover:bg-gray-800">
-              Join the waiting list
+              {buttonLabel}
             </button>
           </div>
         </div>
